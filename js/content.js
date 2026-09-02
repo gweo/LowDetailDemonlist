4b2c1e51ad032e18e3b8fd61db7696202d1d3f6b
gweo <hallbodie1@gmail.com>
2026-09-02 11:47:33 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,8 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/data';
+const dir = '/LowDetailDemonlist/data';
+
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
