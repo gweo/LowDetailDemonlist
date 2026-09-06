a3223eba238bff95db343ff3b2d1e341356be37c
gweo <hallbodie1@gmail.com>
2026-09-06 17:06:26 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
     */
     // New formula
     let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 3)));
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
