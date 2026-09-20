020a8e85f1e282b2f0a2efc74cd4f8afac064127
jacobdigma <guyr0820@gmail.com>
2026-09-20 19:46:34 +0200

    Update ranking display logic in List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,8 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-if="level.unrated" class="type-label-lg">–</p>
+                            <p v-else-if="i + 1 <= 100" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
